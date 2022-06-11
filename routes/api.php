161157055275eb653d97baa2953d7724341253ab
Sakib Bin Ehsan <sakibehsan13@gmail.com>
2022-06-11 21:33:46 +0600

    Created UnitController and corresponding routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWT\JWTController;
 use App\Http\Controllers\API\Admin\HomeController;
 use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\UnitController;
 
 Route::group(['middleware' => 'api'], function ($router) {
     Route::post('/register', [JWTController::class, 'register']);
@@ -12,11 +13,20 @@ Route::group(['middleware' => 'api'], function ($router) {
     Route::post('/logout', [JWTController::class, 'logout']);
     Route::post('/refresh', [JWTController::class, 'refresh']);
     // Route::post('/profile', [JWTController::class, 'profile']);
+
+    /* -------- Product Related Routes -------- */
     Route::get('product', [ProductController::class, 'index']);
     Route::get('product/{id}', [ProductController::class, 'getById']);
     Route::get('product/delete/{id}', [ProductController::class, 'delete']);
     Route::post('product/store', [ProductController::class, 'store']);
     Route::post('product/update', [ProductController::class, 'update']);
+
+    /* -------- Unit Related Routes  -------- */
+    Route::get('unit', [UnitController::class, 'index']);
+    Route::get('unit/{id}', [UnitController::class, 'getById']);
+    Route::post('unit/update', [UnitController::class, 'update']);
+    Route::post('unit/store', [UnitController::class, 'store']);
+    Route::delete('unit/delete/{id}', [UnitController::class, 'delete']);
 });
 
 Route::group(['middleware' => ['jwt.verify']], function () {
