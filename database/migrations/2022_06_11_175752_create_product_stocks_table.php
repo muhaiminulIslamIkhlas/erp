f5af8e7f6995fff549580a0befe73c2876e7a3b4
Sakib Bin Ehsan <sakibehsan13@gmail.com>
2022-06-12 00:13:38 +0600

    Created product-stock controller and corresponding routes

---
+++ database/migrations/2022_06_11_175752_create_product_stocks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_stocks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('qty');
+            $table->integer('buying_price');
+            $table->integer('selling_price');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_stocks');
+    }
+}
