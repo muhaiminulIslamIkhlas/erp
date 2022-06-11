1d23e510e6043c7bd4bb05c0c5dd53a3b7d99ddb
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2022-06-11 13:03:00 +0600

    complete product

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $product = Product::all();
+
+        // return new ProductResource(Product::all());
+
+        return response()->json([
+            'data' => $product
+        ], 202);
+    }
+
+    public function getById($id)
+    {
+        $product = Product::find($id);
+        return response()->json([
+            'data' => $product
+        ], 200);
+    }
+
+    public function storeData(Request $request)
+    {
+        if ($request->id) {
+            $product = Product::find($request->id);
+        } else {
+            $product = new Product();
+        }
+        $product->product_name = $request->product_name;
+        $product->company_name = $request->company_name;
+        $product->size = $request->size;
+        $product->color = $request->color;
+        $product->unit_id = $request->unit_id;
+        $product->save();
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $this->storeData($request);
+            return response()->json([
+                'message' => 'Product saved successfully'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json($th, 500);
+        }
+    }
+
+    public function update(Request $request)
+    {
+        try {
+            $this->storeData($request);
+            return response()->json([
+                'message' => 'Product updated successfully'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json($th, 500);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $product = Product::find($id);
+            $product->delete();
+            return response()->json([
+                'message' => 'Product deleted successfully'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,22 +4,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWT\JWTController;
 use App\Http\Controllers\API\Admin\HomeController;
+use App\Http\Controllers\API\ProductController;
 
-Route::group(['middleware' => 'api'], function($router) {
+Route::group(['middleware' => 'api'], function ($router) {
     Route::post('/register', [JWTController::class, 'register']);
     Route::post('/login', [JWTController::class, 'login']);
     Route::post('/logout', [JWTController::class, 'logout']);
     Route::post('/refresh', [JWTController::class, 'refresh']);
     // Route::post('/profile', [JWTController::class, 'profile']);
+    Route::get('product', [ProductController::class, 'index']);
+    Route::get('product/{id}', [ProductController::class, 'getById']);
+    Route::get('product/delete/{id}', [ProductController::class, 'delete']);
+    Route::post('product/store', [ProductController::class, 'store']);
+    Route::post('product/update', [ProductController::class, 'update']);
 });
 
 Route::group(['middleware' => ['jwt.verify']], function () {
-    
+
     //     Route::post('logout', 'AuthController@logout');
     //     Route::post('refresh', 'AuthController@refresh');
     Route::post('/profile', [JWTController::class, 'profile']);
-    
-    });
+});
 
 /*
 |--------------------------------------------------------------------------
