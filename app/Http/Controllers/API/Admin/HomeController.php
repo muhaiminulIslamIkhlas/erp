6b8d6cc1aecc816b1f6730a58b8de9eae000ebbd
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2022-06-11 12:05:02 +0600

    test

--- app/Http/Controllers/API/Admin/HomeController.php
+++ app/Http/Controllers/API/Admin/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function index()
     {
         return response()->json([
-            'data' => 'test complete'
+            'data' => 'test'
         ]);
     }
 }
