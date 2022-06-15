3260a33d01c4d54761b7377f2af7aa26e0e3c312
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2022-06-15 21:18:51 +0600

    change laravel version

--- routes/api.php
+++ routes/api.php
@@ -2,24 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JWT\JWTController;
-use App\Http\Controllers\API\Admin\HomeController;
-
-Route::group(['middleware' => 'api'], function($router) {
-    Route::post('/register', [JWTController::class, 'register']);
-    Route::post('/login', [JWTController::class, 'login']);
-    Route::post('/logout', [JWTController::class, 'logout']);
-    Route::post('/refresh', [JWTController::class, 'refresh']);
-    // Route::post('/profile', [JWTController::class, 'profile']);
-});
-
-Route::group(['middleware' => ['jwt.verify']], function () {
-    
-    //     Route::post('logout', 'AuthController@logout');
-    //     Route::post('refresh', 'AuthController@refresh');
-    Route::post('/profile', [JWTController::class, 'profile']);
-    
-    });
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +14,6 @@ Route::group(['middleware' => ['jwt.verify']], function () {
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
