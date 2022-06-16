39377f632c225dcd472233d4987f2518cfabbd8a
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2022-06-16 22:03:02 +0600

    added model

---
+++ database/migrations/2022_06_16_155353_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_id');
+            $table->string('customer_name');
+            $table->string('phone')->unique();
+            $table->string('address')->nullable();
+            $table->string('added_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
