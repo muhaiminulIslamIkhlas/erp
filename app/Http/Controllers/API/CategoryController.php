f1e039e0a969c8464d496a8a1b65a34382ca9d8d
nazmulkst <nazmulkst@gmail.com>
2022-06-18 00:31:02 +0600

    Brand, Category, Supplier

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -4,8 +4,161 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Category;
+
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+        try {
+            $category = Category::all();
+            
+            return response()->json(
+                [
+                    'data' => $category
+                ], 200
+            );
+
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'error' => $th->getMessage()
+                ],
+                500
+            );
+        }
+    }
+
+    public function storeData(Request $request){
+        if($request->id){
+            $category = Category::find($request->id);
+        } else {
+            $category = new Category();
+        }
+        $category->store_id = 1;
+        $category->category_name = $request->category_name;
+        $category->added_by = $request->added_by;
+        $category->description = $request->description;
+        $category->save();
+    }
+
+    public function create(Request $request){
+        $validatedData = Validator::make($request->all(), [
+            'category_name' => 'required|max:255',
+            'added_by' => 'required',
+        ]);
+
+        if ($validatedData->fails()) {
+            return response()->json([
+                'data' => [
+                    'error' => $validatedData->errors()
+                ]
+            ], 400);
+        }
+
+        try {
+            $this->storeData($request);
+
+            return response()->json(
+                [
+                    'data' => 'Inserted successfully'
+                ],
+                200
+            );
+
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'data' => [
+                        'error' => $th->getMessage()
+                    ]
+                ],
+                500
+            );
+        }
+    }
+
+    public function edit(Request $request){
+        $validatedData = Validator::make($request->all(), [
+            'category_name' => 'required|max:255',
+            'added_by' => 'required',
+            'id' => 'required'
+        ]);
+
+        if ($validatedData->fails()) {
+            return response()->json([
+                'data' => [
+                    'error' => $validatedData->errors()
+                ]
+            ], 400);
+        }
+
+        try {
+            $this->storeData($request);
+
+            return response()->json(
+                [
+                    'data' => 'Updated successfully'
+                ],
+                200
+            );
+
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'data' => [
+                        'error' => $th->getMessage()
+                    ]
+                ],
+                500
+            );
+        }
+
+    }
+
+    public function delete($id){
+        try {
+            $category = Category::find($id);
+            $category->delete();
+
+            return response()->json(
+                [
+                    'data' => 'Deleted Successfully'
+                ],
+                200
+            );
+        } catch(\Throwable $th){
+            return response()->json(
+                [
+                    'data' => [
+                        'error' => $th->getMessage()
+                    ]
+                ],
+                500
+            );
+        }
+    }
+
+    public function getItem($id){
+        try {
+            $category = Category::find($id);
+
+            return response()->json(
+                [
+                    'data' => $category
+                ],
+                200
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'data' => [
+                        'error' => $th->getMessage()
+                    ]
+                ],
+                500
+            );
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -32,3 +32,30 @@ Route::get('/delete-account/{id}','API\AccountController@delete');
 Route::post('/create-account','API\AccountController@create');
 Route::post('/edit-account','API\AccountController@edit');
 Route::post('/edit-account','API\AccountController@edit');
+
+/**
+ * Brand
+ */
+Route::get('/get-all-brand', 'API\BrandController@index');
+Route::post('/create-brand', 'API\BrandController@create');
+Route::post('/edit-brand', 'API\BrandController@edit');
+Route::get('/delete-brand/{id}', 'API\BrandController@delete');
+Route::get('/get-brand-byId/{id}', 'API\BrandController@getItem');
+
+/**
+ * Category
+ */
+Route::get('/get-all-category', 'API\CategoryController@index');
+Route::post('/create-category', 'API\CategoryController@create');
+Route::post('/edit-category', 'API\CategoryController@edit');
+Route::get('/delete-category/{id}', 'API\CategoryController@delete');
+Route::get('/get-category-byId/{id}', 'API\CategoryController@getItem');
+
+/**
+ * Supplier
+ */
+Route::get('/get-all-supplier', 'API\SupplierController@index');
+Route::post('/create-supplier', 'API\SupplierController@create');
+Route::post('/edit-supplier', 'API\SupplierController@edit');
+Route::get('/delete-supplier/{id}', 'API\SupplierController@delete');
+Route::get('/get-supplier-byId/{id}', 'API\SupplierController@getItem');
